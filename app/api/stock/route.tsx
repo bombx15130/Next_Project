760b66b917fd548c750623c87327ee959d90f518
paul <>
2023-10-18 17:41:11 +0800

    [fix] typescript error

--- app/api/stock/route.tsx
+++ app/api/stock/route.tsx
@@ -1,6 +1,8 @@
+import { NextResponse } from 'next/server'
+
 export async function GET() {
   const res = await fetch('https://openapi.taifex.com.tw/v1/SingleStockFuturesMargining')
   const data = await res.json()
  
-  return Response.json(data)
+  return NextResponse.json(data)
 }
